7a2d21a756f733945786604022670c0206ee63b9
joaop21 <jpsilva9898@gmail.com>
2022-05-21 03:35:28 +0100

    Run prettier

--- packages/contracts/contracts/Bean.sol
+++ packages/contracts/contracts/Bean.sol
@@ -4,7 +4,35 @@ pragma solidity ^0.8.0;
 import "./IBean.sol";
 
 contract Bean is IBean {
+    ///
+    /// state
+    ///
 
-  
+    string coffeeBean;
+    string process;
+    string varietal;
+    uint256 weightInKg;
+    string harvestDate;
 
+    uint256 creationDate;
+
+    ///
+    /// Constructor
+    ///
+
+    constructor(
+        string memory _coffeeBean,
+        string memory _process,
+        string memory _varietal,
+        uint256 _weightInKg,
+        string memory _harvestDate
+    ) {
+        coffeeBean = _coffeeBean;
+        process = _process;
+        varietal = _varietal;
+        weightInKg = _weightInKg;
+        harvestDate = _harvestDate;
+
+        creationDate = block.timestamp;
+    }
 }
--- packages/contracts/contracts/CoffeeLine.sol
+++ packages/contracts/contracts/CoffeeLine.sol
@@ -4,82 +4,91 @@ pragma solidity ^0.8.0;
 import "./ICoffeeLine.sol";
 
 contract CoffeeLine is ICoffeeLine {
+    ///
+    /// State
+    ///
 
-  ///
-  /// State
-  ///
+    mapping(address => Producer) producers;
 
-  mapping(address => Producer) producers;
+    mapping(address => Roaster) roasters;
 
-  mapping(address => Roaster) roasters;
+    ///
+    /// Constructor
+    ///
 
-  ///
-  /// Constructor
-  ///
+    constructor() {}
 
-  constructor() {}
+    ///
+    /// Public API
+    ///
 
-  ///
-  /// Public API
-  ///
+    function createProducer(ProducerParams calldata _params)
+        external
+        override(ICoffeeLine)
+        returns (Producer memory producer)
+    {
+        require(_producerExists(), "Producer already exists");
 
-  function createProducer(ProducerParams calldata _params) external override(ICoffeeLine) returns(Producer memory producer) {
-    require(_producerExists(), "Producer already exists");
+        string[] memory images;
+        IBean[] memory beans;
 
-    string[] memory images;
-    IBean[] memory beans;
+        producer = Producer({
+            name: _params.name,
+            farmName: _params.farmName,
+            farmAddress: _params.farmAddress,
+            country: _params.country,
+            email: _params.email,
+            mobileNumber: _params.mobileNumber,
+            farmSizeSquareMeters: _params.farmSizeSquareMeters,
+            farmMASL: _params.farmMASL,
+            numberOfPeople: _params.numberOfPeople,
+            description: _params.description,
+            images: images,
+            beans: beans
+        });
 
-    producer = Producer({
-      name: _params.name,
-      farmName: _params.farmName,
-      farmAddress: _params.farmAddress,
-      country: _params.country,
-      email: _params.email,
-      mobileNumber: _params.mobileNumber,
-      farmSizeSquareMeters: _params.farmSizeSquareMeters,
-      farmMASL: _params.farmMASL,
-      numberOfPeople: _params.numberOfPeople,
-      description: _params.description,
-      images: images,
-      beans: beans
-    });
+        producers[msg.sender] = producer;
+    }
 
-    producers[msg.sender] = producer;
-  }
+    function createBean(BeanParams calldata _params)
+        external
+        override(ICoffeeLine)
+        returns (IBean bean)
+    {}
 
-  function createBean(BeanParams calldata _params) external override(ICoffeeLine) returns(IBean bean) {
+    function createRoaster(RoasterParams calldata _params)
+        external
+        override(ICoffeeLine)
+        returns (Roaster memory roaster)
+    {
+        require(_roasterExists(), "Roaster already exists");
 
-  }
+        IBean[] memory beans;
 
-  function createRoaster(RoasterParams calldata _params) external override(ICoffeeLine) returns(Roaster memory roaster) {
-    require(_roasterExists(), "Roaster already exists");
+        roaster = Roaster({
+            companyName: _params.companyName,
+            companyAddress: _params.companyAddress,
+            country: _params.country,
+            email: _params.email,
+            mobileNumber: _params.mobileNumber,
+            equipment: _params.equipment,
+            description: _params.description,
+            image: _params.image,
+            beans: beans
+        });
 
-    IBean[] memory beans;
+        roasters[msg.sender] = roaster;
+    }
 
-    roaster = Roaster({
-      companyName: _params.companyName,
-      companyAddress: _params.companyAddress,
-      country: _params.country,
-      email: _params.email,
-      mobileNumber: _params.mobileNumber,
-      equipment: _params.equipment,
-      description: _params.description,
-      image: _params.image,
-      beans: beans
-    });
+    ///
+    /// Private Functions
+    ///
 
-    roasters[msg.sender] = roaster;
-  }
+    function _producerExists() private view returns (bool) {
+        return bytes(producers[msg.sender].name).length > 0;
+    }
 
-  ///
-  /// Private Functions
-  ///
-
-  function _producerExists() private view returns (bool) {
-    return bytes(producers[msg.sender].name).length > 0;
-  }
-
-  function _roasterExists() private view returns (bool) {
-    return bytes(roasters[msg.sender].companyName).length > 0;
-  }
+    function _roasterExists() private view returns (bool) {
+        return bytes(roasters[msg.sender].companyName).length > 0;
+    }
 }
