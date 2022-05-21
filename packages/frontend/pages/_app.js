2b9a7b61957dbc21f09cf9e3b66e9521bbe5f919
Nuno Bernardes <nuno.jmbernardes@gmail.com>
2022-05-21 15:25:55 -0400

    another one

--- packages/frontend/pages/_app.js
+++ packages/frontend/pages/_app.js
@@ -4,32 +4,28 @@ import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    import('bootstrap/dist/js/bootstrap');
-  }, []);
-
   return (
     <>
       <Head>
         <link
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
+          href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css'
           rel='stylesheet'
-          integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3'
-          crossOrigin='anonymous'
-        />
-        <link
-          rel='stylesheet'
-          href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css'
-          integrity='sha384-eoTu3+HydHRBIjnCVwsFyCpUDZHZSFKEJD0mc3ZqSBSb6YhZzRHeiomAUWCstIWo'
+          integrity='sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor'
           crossorigin='anonymous'
-        ></link>
+        />
+        <link rel='icon' type='image/x-icon' href='/favicon.png' />
       </Head>
       <Script
-        id='bootstrap-cdn'
         src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'
         integrity='sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p'
+        crossOrigin='anonymous'
       />
-      <Component id='home' {...pageProps} />
+      <Script
+        src='https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js'
+        integrity='sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk'
+        crossOrigin='anonymous'
+      />
+      <Component {...pageProps} />
     </>
   );
 }
--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -1,80 +1,93 @@
 import React from 'react';
 import Image from 'next/image';
+import Head from 'next/head';
+import { useContractKit } from '@celo-tools/use-contractkit';
 import { ContractKitProvider } from '@celo-tools/use-contractkit';
 import '@celo-tools/use-contractkit/lib/styles.css';
 
 function App() {
+  const { address, connect } = useContractKit();
+
   return (
-    <div className='container'>
-      <nav className='navbar fixed-top'>
-        <div className='container'>
-          <a href='index.html' className='logo'>
-            <Image alt='Coffeline' src='/logo.svg' width={100} height={40} />
-          </a>
+    <>
+      <Head>
+        <title>Coffeeline</title>
+      </Head>
 
-          <button
-            className='navbar-toggler'
-            type='button'
-            data-bs-toggle='offcanvas'
-            data-bs-target='#offcanvasNavbar'
-            aria-controls='offcanvasNavbar'
-          >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-          <div
-            className='offcanvas offcanvas-end'
-            tabIndex='-1'
-            id='offcanvasNavbar'
-            aria-labelledby='offcanvasNavbarLabel'
-          >
-            <div className='offcanvas-header'>
-              <button
-                type='button'
-                className='btn-close'
-                data-bs-dismiss='offcanvas'
-                aria-label='Close'
-              ></button>
-            </div>
-            <div className='offcanvas-body'>
-              <ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
-                <li className='nav-item'>
-                  <a
-                    className='nav-link active'
-                    aria-current='page'
-                    href='index.html'
+      <div className='container'>
+        <nav className='navbar fixed-top'>
+          <div className='container'>
+            <a href='index.html' className='logo'>
+              <Image alt='Coffeline' src='/logo.svg' width={100} height={40} />
+            </a>
+
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-bs-toggle='offcanvas'
+              data-bs-target='#offcanvasNavbar'
+              aria-controls='offcanvasNavbar'
+            >
+              <span className='navbar-toggler-icon' />
+            </button>
+            <div
+              className='offcanvas offcanvas-end'
+              tabIndex='-1'
+              id='offcanvasNavbar'
+              aria-labelledby='offcanvasNavbarLabel'
+            >
+              <div className='offcanvas-header'>
+                <button
+                  type='button'
+                  className='btn-close'
+                  data-bs-dismiss='offcanvas'
+                  aria-label='Close'
+                ></button>
+              </div>
+              <div className='offcanvas-body'>
+                <ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
+                  <li className='nav-item'>
+                    <a
+                      className='nav-link active'
+                      aria-current='page'
+                      href='index.html'
+                    >
+                      <h3>Home</h3>
+                    </a>
+                  </li>
+                  <li className='nav-item'>
+                    <a className='nav-link' href='#'>
+                      <h3>How does it work?</h3>
+                    </a>
+                  </li>
+                </ul>
+
+                <div className='line'></div>
+
+                <a onClick={connect}>
+                  <button
+                    data-bs-dismiss='offcanvas'
+                    className='primary-button'
                   >
-                    <h3>Home</h3>
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' href='#'>
-                    <h3>How does it work?</h3>
-                  </a>
-                </li>
-              </ul>
-
-              <div className='line'></div>
-
-              <a href='connectwallet.html'>
-                <button className='primary-button'>
-                  <h3>Connect wallet</h3>
-                </button>
-              </a>
+                    <h3>Connect wallet</h3>
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
 
-      <section id='intro'>
-        <h2 className='lora'>
-          Check the <span>history</span> of your special coffee.
-        </h2>
+        <section id='intro'>
+          <h2 className='lora'>
+            Check the <span>history</span> of your special coffee.
+          </h2>
 
-        <div className='scan'></div>
+          <div className='scan'></div>
 
-        <p className='small-caption'>Scan QR code</p>
-      </section>
-    </div>
+          <p className='small-caption'>Scan QR code</p>
+        </section>
+      </div>
+    </>
   );
 }
 
